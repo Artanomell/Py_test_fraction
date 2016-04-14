f10eb255fe3cecade57cdafb420168cca08f79dc
alexeyrud <rud.alexey@gmail.com>
2016-04-14 09:42:21 +0300

    Adding support for negative numbers and check for 0-values

--- fractions.py
+++ fractions.py
@@ -12,23 +12,37 @@ print inst3
 # 7/6
 """
 
+
 class fraction:
     def __init__(self, numerator, denominator):
-        self.numerator = numerator
-        self.denominator = denominator
+        if denominator == 0:
+            raise ('Floating by zero')
+
+        if denominator < 0:
+            self.denominator = -1 * denominator
+            self.numerator = -1 * numerator
+        else:
+            self.numerator = numerator
+            self.denominator = denominator
 
     def __str__(self):
-        return '{}/{}'.format(self.numerator, self.denominator)
+        return '{0}/{1}'.format(self.numerator, self.denominator)
 
     def __add__(self, other):
-        common_denominator = fraction.__lcm(self, self.denominator, other.denominator)
-        num1 = int(self.numerator * common_denominator / self.denominator)
-        num2 = int(other.numerator * common_denominator / other.denominator)
-        return fraction(num1 + num2, common_denominator)
+        if self.numerator == 0 and other.numerator != 0:
+            return other
+        elif other.numerator == 0 and self.numerator != 0:
+            return self
+        else:
+            common_denominator = fraction.__lcm(self, self.denominator, other.denominator)
+            num1 = int(self.numerator * common_denominator / self.denominator)
+            num2 = int(other.numerator * common_denominator / other.denominator)
+            return fraction(num1 + num2, common_denominator)
 
     def __lcm(self, a, b):
         """
         Приватный метод нахождения наименьшего общего кратного целых чисел a и b
+        честно утянул с http://younglinux.info/python/task/lcm
         :param a:
         :param b:
         :return:
